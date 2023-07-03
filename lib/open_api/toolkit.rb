701e4cfbe32848fc6441d9ef59cd46e3b92e76f7
Nate Klaiber <nate@theklaibers.com>
2023-07-03 11:22:57 -0400

    initial

---
+++ lib/open_api/toolkit.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "toolkit/version"
+
+module OpenApi
+  module Toolkit
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
