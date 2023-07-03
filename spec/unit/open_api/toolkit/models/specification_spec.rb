52b258708e079b757dee9b0df7d83f5c336f0850
Nate Klaiber <nate@theklaibers.com>
2023-07-03 11:55:01 -0400

    Base spec of expectations and returns

--- spec/unit/open_api/toolkit/models/specification_spec.rb
+++ spec/unit/open_api/toolkit/models/specification_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe(OpenApi::Toolkit::Models::Specification) do
   let(:model_attributes) do
     {
-      :openapi => '3.1.0'
+      :openapi => '3.1.0',
     }
   end
 
@@ -10,6 +10,30 @@ RSpec.describe(OpenApi::Toolkit::Models::Specification) do
   context "with valid values" do
     it "returns the OpenAPI version" do
       expect(subject.openapi).to eq('3.1.0')
+    end
+
+    it "returns the info attributes" do
+      expected = {}
+
+      expect(subject.info_attributes).to eq(expected)
+    end
+
+    it "returns the servers attributes" do
+      expected = []
+
+      expect(subject.servers_attributes).to eq(expected)
+    end
+
+    it "returns the paths attributes" do
+      expected = []
+
+      expect(subject.paths_attributes).to eq(expected)
+    end
+
+    it "returns the tags attributes" do
+      expected = []
+
+      expect(subject.tags_attributes).to eq(expected)
     end
   end
 end
